10d20622a63bc057b86a407e1473ef8b41c9a151
Ludmila <lvminzulina@mail.ru>
2022-10-29 22:25:59 +0300

    2

--- task.js
+++ task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  
 }
+let Oleg = new Student("Oleg", "man", 23);
+let Anton = new Student("Антон", "man", 40);
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
-
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) {
+    Student.prototype.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+ 
+Student.prototype.addMarks = function(...mark) {
+  if (this.marks === undefined) {
+    Student.prototype.marks = [];
+    this.marks.push(...mark);
+  } else {
+    this.marks.push(...mark);
+  }
+}
+ 
+Student.prototype.getAverage = function() {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  return sum / this.marks.length;;
+}
+ 
+Student.prototype.exclude = function(reason) {
+  this.reason = reason;
+  delete this.subject;
+  delete this.marks;
+}
